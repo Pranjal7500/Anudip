b9c4b5ce2647c8f22981d624f04d913c9c701075
Pranjal7500 <117526302+Pranjal7500@users.noreply.github.com>
2025-05-06 19:42:36 +0530

    Add files via upload
    To calculate the Simple interest

---
+++ Simple Interest.py
@@ -0,0 +1,7 @@
+
+#simple Intrest 
+Principle_amount=float(input("enter the Principle amount:"))
+Rate_of_interest=float(input("enter the Rate_of_interest:"))
+Time=int(input("enter the timr in year:"))
+Simple Interest =p*t*r/100
+print(Simple Interest)
